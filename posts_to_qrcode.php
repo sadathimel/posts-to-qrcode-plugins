fa803cb5f5960995ca86d0d1085d5999da50c576
Mahadi001 <sadat.himel@gmail.com>
2021-06-06 16:00:30 +0600

    update

--- posts_to_qrcode.php
+++ posts_to_qrcode.php
@@ -9,8 +9,7 @@ Author URI: http://github.com/sadathimel
 License: GPLv2 or later
 Text Domain: posts-to-qrcode
 Domain Path: /languages
-*/
-
+ */
 
 // function qrcode_activation_hook(){
 
@@ -22,17 +21,25 @@ Domain Path: /languages
 // }
 // register_deactivation_hook(__FILE__,"qrcode_deactivation_hook");
 
-function posts_to_qrcode_load_textdomain(){
-    load_plugin_textdomain('posts-to-qrcode', false, dirname(__FILE__) . "/languages");
+function posts_to_qrcode_load_textdomain() {
+    load_plugin_textdomain( 'posts-to-qrcode', false, dirname( __FILE__ ) . "/languages" );
 }
 add_action( 'plugins_loaded', 'posts_to_qrcode_load_textdomain' );
 
-function pqrc_display_qr_code($content){
-    $current_post_id = get_the_ID();
-    $current_post_title = get_the_title($current_post_id);
-    $current_post_url = urlencode(get_the_permalink($current_post_id));
-    $image_src = sprintf('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=%s',$current_post_url);
-    $content .= sprintf("<div class ='qrcode'><img src='%s' alt='%s' /> </div>",$image_src,$current_post_title);
+function pqrc_display_qr_code( $content ) {
+    $current_post_id    = get_the_ID();
+    $current_post_title = get_the_title( $current_post_id );
+    $current_post_url   = urlencode( get_the_permalink( $current_post_id ) );
+    $current_post_type  = get_post_type( $current_post_id );
+    /**
+     * post type chack
+     */
+    $excluded_post_type = apply_filters( "pqrc_excluded_post_type", [] );
+    if ( in_array( $current_post_type, $excluded_post_type ) ) {
+        return $content;
+    }
+    $image_src = sprintf( 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=%s', $current_post_url );
+    $content .= sprintf( "<div class ='qrcode'><img src='%s' alt='%s' /> </div>", $image_src, $current_post_title );
     return $content;
 }
-add_filter( 'the_content', 'pqrc_display_qr_code');
+add_filter( 'the_content', 'pqrc_display_qr_code' );
