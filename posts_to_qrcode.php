8eda46b802b17f0063f2e07982cf434f2d7471ab
Mahadi001 <sadat.himel@gmail.com>
2021-06-06 13:41:00 +0600

    update

--- posts_to_qrcode.php
+++ posts_to_qrcode.php
@@ -27,9 +27,12 @@ function posts_to_qrcode_load_textdomain(){
 }
 add_action( 'plugins_loaded', 'posts_to_qrcode_load_textdomain' );
 
-function pqrc_display_qr_code($contant){
+function pqrc_display_qr_code($content){
     $current_post_id = get_the_ID();
-    $cuttent_post_url = get_the_permalink($current_post_id);
-    $image_src = urlencode(sprintf('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=%s'));
+    $current_post_title = get_the_title($current_post_id);
+    $current_post_url = urlencode(get_the_permalink($current_post_id));
+    $image_src = sprintf('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=%s',$current_post_url);
+    $content .= sprintf("<div class ='qrcode'><img src='%s' attr='%s' /> </div>",$image_src,$current_post_title);
+    return $content;
 }
 add_filter( 'the_content', 'pqrc_display_qr_code');
