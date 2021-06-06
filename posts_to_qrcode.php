627e91d47af8d96b775a6eafea71eba5d0ad9794
Mahadi001 <sadat.himel@gmail.com>
2021-06-06 13:19:16 +0600

    update

--- posts_to_qrcode.php
+++ posts_to_qrcode.php
@@ -22,3 +22,14 @@ Domain Path: /languages
 // }
 // register_deactivation_hook(__FILE__,"qrcode_deactivation_hook");
 
+function posts_to_qrcode_load_textdomain(){
+    load_plugin_textdomain('posts-to-qrcode', false, dirname(__FILE__) . "/languages");
+}
+add_action( 'plugins_loaded', 'posts_to_qrcode_load_textdomain' );
+
+function pqrc_display_qr_code($contant){
+    $current_post_id = get_the_ID();
+    $cuttent_post_url = get_the_permalink($current_post_id);
+    $image_src = urlencode(sprintf('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=%s'));
+}
+add_filter( 'the_content', 'pqrc_display_qr_code');
