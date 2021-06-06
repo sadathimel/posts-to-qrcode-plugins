5b2729edceaff9674fafd795f74ce1c02cccc334
Mahadi001 <sadat.himel@gmail.com>
2021-06-06 13:51:42 +0600

    update

--- posts_to_qrcode.php
+++ posts_to_qrcode.php
@@ -32,7 +32,7 @@ function pqrc_display_qr_code($content){
     $current_post_title = get_the_title($current_post_id);
     $current_post_url = urlencode(get_the_permalink($current_post_id));
     $image_src = sprintf('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=%s',$current_post_url);
-    $content .= sprintf("<div class ='qrcode'><img src='%s' attr='%s' /> </div>",$image_src,$current_post_title);
+    $content .= sprintf("<div class ='qrcode'><img src='%s' alt='%s' /> </div>",$image_src,$current_post_title);
     return $content;
 }
 add_filter( 'the_content', 'pqrc_display_qr_code');
