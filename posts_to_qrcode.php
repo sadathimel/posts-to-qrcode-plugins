305b05eb783e1afbc0423f4cc3fbe260ee1ca181
Mahadi001 <sadat.himel@gmail.com>
2021-06-06 12:20:06 +0600

    create new plugins
    this is a new plugins . this plugins works from generate posts to qrcode

---
+++ posts_to_qrcode.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Posts To QR Code
+Plugin URI: http://github.com/sadathimel
+Description: This plugins Display QR code Under every posts.
+Version: 1.0
+Author: sadat himel
+Author URI: http://github.com/sadathimel
+License: GPLv2 or later
+Text Domain: posts-to-qrcode
+Domain Path: /languages
+*/
+
+
+// function qrcode_activation_hook(){
+
+// }
+// register_activation_hook(__FILE__,"qrcode_activation_hook");
+
+// function qrcode_deactivation_hook(){
+
+// }
+// register_deactivation_hook(__FILE__,"qrcode_deactivation_hook");
+
